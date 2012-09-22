0d206e98962fbd5bf3b8ccdc0f8d30a4e1eb196d
Gabriel Violette <phoniks7@hotmail.com>
2012-09-22 16:33:48 -0400

    Added __init__(), update(), and draw() methods to superclass.

---
+++ superclass.py
@@ -0,0 +1,11 @@
+class SuperClass(object):
+    def __init__(self):
+        """Initialize the superclass object"""
+        super(SuperClass, self).__init__()
+        pass
+    def update(self):
+        """update method"""
+        pass
+    def draw(self):
+        """draw method"""
+        pass
