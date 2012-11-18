dece1c99fc209bd23e83bb5dd57c899a7c98c282
weitht <weitherst@gmail.com>
2012-11-18 18:31:55 -0500

    Added new ice skill sound.

--- game.py
+++ game.py
@@ -77,6 +77,7 @@ class Game(object):
         self.fire_skill_sound = pygame.mixer.Sound("Music/fire.ogg")
         self.fire_skill_sound.set_volume(.7)
         self.ice_skill_sound = pygame.mixer.Sound("Music/ice.ogg")
+        self.ice_skill_sound.set_volume(.9)
         self.lightning_skill_sound = pygame.mixer.Sound("Music/lightning.ogg")
         self.poison_skill_sound = pygame.mixer.Sound("Music/poison.ogg")
         self.missile_sound = pygame.mixer.Sound("Music/turretattack.ogg")
