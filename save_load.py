8833f213a3027f19767d0ab7bf79c393f6fe85db
Paul Tetreau <tetrep@gmail.com>
2012-11-18 20:27:09 -0500

    creeps give xp and gold

--- save_load.py
+++ save_load.py
@@ -35,3 +35,7 @@ class SaveLoad(object):
 
         #we're done with the file
         file_in.close()
+
+    ## the save game function
+    #  @brief saves the game
+    def save_game(self, game, filename = "test"):
